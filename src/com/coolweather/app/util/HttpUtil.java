9f8fe8b1feca6a147417793bc8f92c2a772e7a11
Rao <raonaxin@163.com>
2016-08-14 22:17:56 +0800

    新增切换城市和手动更新天气的功能。更换图标。

--- src/com/coolweather/app/util/HttpUtil.java
+++ src/com/coolweather/app/util/HttpUtil.java
@@ -1,10 +1,15 @@
 package com.coolweather.app.util;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URL;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import android.util.Log;
 
 public class HttpUtil {
 	
@@ -14,25 +19,37 @@ public class HttpUtil {
 			@Override
 			public void run() {
 				HttpURLConnection connection = null;
+				Log.d("HttpUtil", "address = " + address.toString());
 				try {
-					URL url = new URL(address);
-					connection = (HttpURLConnection) url.openConnection();
-					connection.setRequestMethod("GET");
-					connection.setConnectTimeout(8000);
-					connection.setReadTimeout(8000);
-					InputStream in = connection.getInputStream();
-					BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-					StringBuilder response = new StringBuilder();
-					String line;
-					while ((line = reader.readLine()) != null) {
-						response.append(line);
+//					URL url = new URL(address);
+//					connection = (HttpURLConnection) url.openConnection();
+//					connection.setRequestMethod("GET");
+//					connection.setConnectTimeout(8000);
+//					connection.setReadTimeout(8000);
+//					InputStream in = connection.getInputStream();
+//					BufferedReader reader = new BufferedReader(new InputStreamReader(in, "GB2312"));
+//					StringBuilder response = new StringBuilder();
+//					String line;
+//					while ((line = reader.readLine()) != null) {
+//						response.append(line);
+//					}
+
+					HttpClient httpClient = new DefaultHttpClient();
+					HttpGet httpGet = new HttpGet(address);
+					HttpResponse httpResponse = httpClient.execute(httpGet);
+					String response = "";
+					if (httpResponse.getStatusLine().getStatusCode() == 200) {
+						HttpEntity entity = httpResponse.getEntity();
+						response = EntityUtils.toString(entity, "utf-8");
 					}
+					Log.d("HttpUtil", "listener");
 					if (listener != null) {
 						listener.onFinish(response.toString());
 					}
 				} catch (Exception e) {
 					if (listener != null) {
 						listener.onError(e);
+						Log.d("HttpUtil", e.toString());
 					}
 				} finally {
 					if (connection != null) {
